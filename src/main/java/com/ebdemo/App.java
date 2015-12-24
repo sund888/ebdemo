d143c5a5f657e42735bbee8f562181741546999b
Sun <sdang@playnetwork.com>
2015-12-24 08:21:19 -0800

    new files

---
+++ src/main/java/com/ebdemo/App.java
@@ -0,0 +1,13 @@
+package com.ebdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
